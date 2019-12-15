d4bbbb9d81fbc492dbd3d3c77ef81f3daf524467
root <root@localhost.localdomain>
2019-12-15 07:15:04 -0500

    Add CORS header and update docker-compose

--- request_log/app.py
+++ request_log/app.py
@@ -14,6 +14,24 @@ app.register_blueprint(ui, url_prefix = UI_PREFIX)
 
 
 @app.after_request
+def set_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+
+    if request.method == "OPTIONS":
+        # Both of these headers are only used for the "preflight request"
+        # http://www.w3.org/TR/cors/#access-control-allow-methods-response-header
+        response.headers[
+            "Access-Control-Allow-Methods"
+        ] = "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+        response.headers["Access-Control-Max-Age"] = "3600"  # 1 hour cache
+        if request.headers.get("Access-Control-Request-Headers") is not None:
+            response.headers["Access-Control-Allow-Headers"] = request.headers[
+                "Access-Control-Request-Headers"
+            ]
+    return response
+
+@app.after_request
 def log_request(response):
 
     if request.path.startswith(UI_PREFIX) or request.path.startswith(STATIC_PREFIX):
